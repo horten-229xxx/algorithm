e91a9a7c426b224f9043de7a668815330b9ac2eb
xxxBW <844824960@qq.com>
2016-03-15 20:23:50 +0800

    1与‘1’的区别
    第一个函数中是去重中1跟‘1’是当做同一个元素对待，第二种是利用类型和值判断的，第三种是Rbin给出的一种解法~

--- arrayRepeat.js
+++ arrayRepeat.js
@@ -20,3 +20,30 @@ function normalize(arr) {
 		console.log('参数为非数组！');
 	}
 }
+
+function normalize1(arr) {
+	for(var i = 0;i < len;i++) { 
+ 	// 利用类型和数值做键值 
+	 	var key = typeof(arr[i]) + arr[i]; 
+	 	if(!obj[key]) { 
+	 	// obj中不存在该属性则添加并且把字段加入到str中 
+		 	obj[key] = true; 
+		 	str.push(arr[i]); 
+	 	} 
+	} 
+}
+
+/*
+* 来自Rbin的点拨
+*/
+function normalize2(arr) {
+	var str=[],obj={};
+	arr.forEach(function(item,index){
+		// 把数组值转换为字符串，因为obj[key]会去掉字符串中的引号
+		var key = JSON.stringify(item);
+	  	if( !obj[key] ){
+		 	obj[key] = true;
+		 	str.push(item);
+		}
+	});
+}
